570b763e03fc8f80185243ed2a921d9432dbad57
Zyst <zyst@gmx.com>
2015-10-07 15:32:46 -0500

    Debugger fun project

---
+++ 2.- Crawling/DailyRate/DailyRate/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DailyRate
+{
+    /// <summary>
+    /// While this entire "program" is extremely simple the main use of this exercise was 
+    /// getting used to the Visual Studio debugger. 
+    /// 
+    /// Right now I'm using Visual Studio 2015, this debug sidebar really makes me feel like I'm living in the future!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            double dailyRate = readDouble("Enter your daily rate: ");
+
+            int noOfDays = readInt("Enter number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
+        }
+
+        private void writeFee(double v)
+        {
+            Console.WriteLine("The consultant's fee is: {0}", v * 1.1);
+        }
+
+        private double calculateFee(double dailyRate, int noOfDays)
+        {
+            return dailyRate * noOfDays;
+        }
+
+        private int readInt(string v)
+        {
+            Console.Write(v);
+            string line = Console.ReadLine();
+
+            return int.Parse(line);
+        }
+
+        private double readDouble(string v)
+        {
+            Console.Write(v);
+            string line = Console.ReadLine();
+
+            return double.Parse(line);
+        }
+    }
+}
---
+++ 2.- Crawling/PrimitiveDataTypes/PrimitiveDataTypes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveDataTypes
+{
+    /// <summary>
+    /// Data Types Example
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exampleInteger     = 1;
+            long exampleLong       = 1000000;
+            float exampleFloat     = 1.11F;
+            double exampleDouble   = 1.1234567890;
+            decimal exampleDecimal = 1029999912312312515151231231M;
+            string exampleString   = "You're an animal Jim";
+            char exampleChar       = 'z';
+            bool exampleBoolean    = true;
+
+            // We print out the variables in order, with a brief explanation
+            Console.WriteLine("Different Data Types!");
+
+            Console.WriteLine("An Integer is a round number like: " + exampleInteger.ToString());
+
+            Console.WriteLine("A Long is a 64bit integer: " + exampleLong.ToString());
+
+            Console.WriteLine("A Float is a number with decimal points, it has to finish with the letter 'F': " + exampleFloat.ToString());
+
+            Console.WriteLine("A Double is a 64 bit number with decimal points: " + exampleDouble.ToString());
+
+            Console.WriteLine("A Decimal is a turbo long number, it has to finish with the letter 'M': " + exampleDecimal.ToString());
+
+            Console.WriteLine("A string is a sentence between quotation marks \"Like so\": " + exampleString);
+
+            Console.WriteLine("A char is a single letter, it is set between single quotation marks 'a': " + exampleChar);
+
+            Console.WriteLine("A boolean is a 2 bit variable that can be true or false: " + exampleBoolean.ToString());
+
+        }
+    }
+}
