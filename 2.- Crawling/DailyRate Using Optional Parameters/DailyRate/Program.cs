0a3df996c4847e0ea17b03ca0b2db704291a7c0b
Zyst <zyst@gmx.com>
2015-10-09 20:16:28 -0500

    If/else, and booleans

---
+++ 2.- Crawling/DailyRate Using Optional Parameters/DailyRate/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DailyRate
+{
+    /// <summary>
+    /// Overloading and Optional test
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+
+        // Use one of the double fees below for functionality testing
+        void run()
+        {
+            // double fee = calculateFee(); // 400
+
+            // double fee = calculateFee(650.0); // 650
+
+            // double fee = calculateFee(500.0, 3); // 1500
+
+            // double fee = calculateFee(dailyRate: 375.0); // 375
+
+            // double fee = calculateFee(noOfdays: 4); // 2000
+
+            // double fee = calculateFee(theDailyRate: 375.0); // 375. having changed this from 'dailyRate' to 
+                                                              // 'theDailyRate' allows both methods to exist.
+
+            // Use any of the above 
+
+            Console.WriteLine("Fee is {0}", fee);
+        }
+
+        // 'dailyRate' renamed to 'theDailyRate' to test more optionals stuff
+        private double calculateFee(double theDailyRate = 500.0, int noOfdays = 1)
+        {
+            Console.WriteLine("calculateFee using two optional parameters.");
+
+            return theDailyRate * noOfdays;
+        }
+
+        private double calculateFee(double dailyRate = 500.0)
+        {
+            Console.WriteLine("calculateFee using one optional parameter");
+
+            int defaultNoOfDays = 1;
+
+            return dailyRate * defaultNoOfDays;
+        }
+
+        private double calculateFee()
+        {
+            Console.WriteLine("calculateFee using hardcoded values");
+
+            double defaultDailyRate = 400.0;
+            int defaultNoOfDays = 1;
+
+            return defaultDailyRate * defaultNoOfDays;
+        }
+    }
+}
---
+++ 2.- Crawling/Selection/Selection/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Selection
+{
+    /// <summary>
+    /// Interaction logic for Window.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void compareClick(object sender, RoutedEventArgs e)
+        {
+            int diff = dateCompare(first, second);
+            info.Text = "";
+            show("firstDate == secondDate", diff == 0);
+            show("firstDate != secondDate", diff != 0);
+            show("firstDate <  secondDate", diff < 0);
+            show("firstDate <= secondDate", diff <= 0);
+            show("firstDate >  secondDate", diff > 0);
+            show("firstDate >= secondDate", diff >= 0);
+        }
+
+        private void show(string exp, bool result)
+        {
+            info.Text += exp;
+            info.Text += " : " + result.ToString();
+            info.Text += "\n";
+        }
+
+        /// <summary>
+        /// Goes through all the options, and assigns a specific number to result, since we don't
+        /// only wanna know if the dates are the same, we also want to know if it's one is 
+        /// an earlier or a later date.
+        /// 
+        /// We don't use the Date Compare method because this exercise is meant to show if/else if and boolean statements in general.
+        /// </summary>
+        /// <param name="leftHandSide">The first date parameter we pass, located on the left side of our program.</param>
+        /// <param name="rightHandSide">The second date parameter we pass, located on the right side of our program.</param>
+        /// <returns></returns>
+        private int dateCompare(DateTime leftHandSide, DateTime rightHandSide)
+        {
+            int result;
+
+            if (leftHandSide.Year < rightHandSide.Year)
+            {
+                result = -1;
+            }
+            else if (leftHandSide.Year > rightHandSide.Year)
+            {
+                result = 1;
+            }
+            else if (leftHandSide.Month < rightHandSide.Month)
+            {
+                result = -1;
+            }
+            else if (leftHandSide.Month > rightHandSide.Month)
+            {
+                result = 1;
+            }
+            else if (leftHandSide.Day < rightHandSide.Day)
+            {
+                result = -1;
+            }
+            else if (leftHandSide.Day > rightHandSide.Day)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+
+        #region Conversion properties to cater for the deifferences betwwen the DatePicker control in Windows 7 and Windows 8.1
+        private DateTime first
+        {
+            get { return this.firstDate.SelectedDate.GetValueOrDefault(this.firstDate.DisplayDate); }
+        }
+
+        private DateTime second
+        {
+            get { return this.secondDate.SelectedDate.GetValueOrDefault(this.secondDate.DisplayDate); }
+        }
+        #endregion
+    }
+}
