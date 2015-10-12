6e83356b1c2785c42186f52b1da6489b14cf2eab
Zyst <zyst@gmx.com>
2015-10-12 01:58:07 -0500

    Classes

---
+++ 3.- Walking/Classes/Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point();
+            Point bottomRight = new Point(1366, 768);
+
+            double distance = origin.DistanceTo(bottomRight);
+
+            Console.WriteLine("Distance between the points is: {0}", distance);
+
+            Console.WriteLine("Number of point objects: {0}", Point.ObjectCount());
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
