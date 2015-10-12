73031f01a671ae921753076b3b7b025134f80e67
Zyst <zyst@gmx.com>
2015-10-12 02:44:13 -0500

    Added Parameters with References, and a few other things

---
+++ 3.- Walking/Parameters/Parameters/Pass.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Parameters
+{
+	class Pass
+	{
+		public static void Value(int param)
+        {
+            param = 42;
+        }
+
+        public static void Reference(WrappedInt param)
+        {
+            param.Number = 42;
+        }
+
+        public static void RefValue(ref int param)
+        {
+            param = 42;
+        }
+	}
+}
---
+++ 3.- Walking/Parameters/Parameters/Program.cs
@@ -0,0 +1,60 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Parameters
+{
+    class Program
+    {
+        static void doWork()
+        {
+            //int i = 0;
+
+            //Console.WriteLine(i); // 0
+
+            //Pass.Value(i);
+
+            //Console.WriteLine(i); // 0: As expected, the value is unchanged
+
+
+            // ----------------------------------------------------------------
+
+
+            //WrappedInt wi = new WrappedInt();
+
+            //Console.WriteLine(wi.Number); // 0
+
+            //Pass.Reference(wi);
+
+            //Console.WriteLine(wi.Number); // 42: Because we are editing the object value.
+
+
+            // ----------------------------------------------------------------
+
+
+            int i = 0;
+
+            Console.WriteLine(i); // 0
+
+            Pass.RefValue(ref i);
+
+            Console.WriteLine(i); // 42: The ref keyword allowed us to edit it directly.
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
