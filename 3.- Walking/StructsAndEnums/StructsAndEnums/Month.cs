cdd93e4da96af9c6ef094a2d33b4e5eac61312b7
Zyst <zyst@gmx.com>
2015-10-12 21:47:47 -0500

    Structures and Enums

---
+++ 3.- Walking/StructsAndEnums/StructsAndEnums/Month.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace StructsAndEnums
+{
+	enum Month
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
---
+++ 3.- Walking/StructsAndEnums/StructsAndEnums/Program.cs
@@ -0,0 +1,66 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        static void doWork()
+        {
+            //Month first = Month.January;
+
+            //Console.WriteLine(first); // January
+
+            //first++; // What, this works?!
+
+            //Console.WriteLine(first); // February
+
+            // ----------------------------------------------------------------
+
+            //Month second = Month.December;
+
+            //Console.WriteLine(second); // December
+
+            //second++;
+
+            //Console.WriteLine(second); // 12
+
+            // ----------------------------------------------------------------
+
+            Date defaultDate = new Date();
+
+            Console.WriteLine(defaultDate); // January 1 1900
+
+            Date weddingAniversary = new Date(2013, Month.July, 4);
+
+            Console.WriteLine(weddingAniversary); // July 4 2013
+
+            Date weddingAniversaryCopy = weddingAniversary;
+
+            Console.WriteLine("\nValue of copy is {0}\n", weddingAniversaryCopy); // Value of copy is July 4 2013
+
+            weddingAniversary.AdvanceMonth();
+
+            Console.WriteLine("New value of weddingAniversary is {0}", weddingAniversary); // New value of weddingAniversary is August 4 2013
+
+            Console.WriteLine("Value of copy is still {0}", weddingAniversaryCopy); // Value of copy is still July 4 2013
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
