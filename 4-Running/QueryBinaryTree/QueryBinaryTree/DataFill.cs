dd657b564413ef1d4b9ea19679750105e7595744
Zyst <zyst@gmx.com>
2015-10-20 04:32:46 -0500

    Edited folder structure, also LINQ

---
+++ 4-Running/QueryBinaryTree/QueryBinaryTree/DataFill.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+
+namespace QueryBinaryTree
+{
+    class DataFill
+    {
+        public static Tree<Employee> FillWithData()
+        {
+            Tree<Employee> tree = new Tree<Employee>
+                (
+                    new Employee
+                    {
+                        Id = 1,
+                        FirstName = "Kim",
+                        LastName = "Abercrombie",
+                        Department = "IT"
+                    }
+                );
+
+            tree.Insert
+                (
+                    new Employee
+                    {
+                        Id = 2,
+                        FirstName = "Jeff",
+                        LastName = "Hay",
+                        Department = "Marketing"
+                    }
+                );
+
+            tree.Insert
+                (
+                    new Employee
+                    {
+                        Id = 4,
+                        FirstName = "Charlie",
+                        LastName = "Herb",
+                        Department = "IT"
+                    }
+                );
+
+            tree.Insert
+                (
+                    new Employee
+                    {
+                        Id = 6,
+                        FirstName = "Chris",
+                        LastName = "Preston",
+                        Department = "Sales"
+                    }
+                );
+
+            tree.Insert
+                (
+                    new Employee
+                    {
+                        Id = 3,
+                        FirstName = "Dave",
+                        LastName = "Barnet",
+                        Department = "Sales"
+                    }
+                );
+
+            tree.Insert
+                (
+                    new Employee
+                    {
+                        Id = 5,
+                        FirstName = "Tim",
+                        LastName = "Litton",
+                        Department = "Marketing"
+                    }
+                );
+
+            return tree;
+        }
+    }
+}
---
+++ 4-Running/QueryBinaryTree/QueryBinaryTree/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BinaryTree;
+
+namespace QueryBinaryTree
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // Fill the information from an encapsulated method.
+            Tree<Employee> empTree = DataFill.FillWithData();
+
+            Console.WriteLine("List of departments");
+
+            var depts = empTree.Select(d => d.Department).Distinct();
+
+            // List of departments
+            // Department: IT
+            // Department: Marketing
+            // Department: Sales
+            foreach (var dept in depts)
+            {
+                Console.WriteLine("Department: {0}", dept);
+            }
+
+            // ----------------------------------------------------------------
+
+            Console.WriteLine("\nEmployees in the IT department");
+
+            var ITEmployees = empTree.Where(e => String.Equals(e.Department, "IT"))
+                                                       .Select(emp => emp);
+
+            // Employees in the IT department
+            // Id: 1, Name: Kim Abercrombie, Dept: IT
+            // Id: 4, Name: Charlie Herb, Dept: IT
+            foreach (var emp in ITEmployees)
+            {
+                Console.WriteLine(emp);
+            }
+
+            // ----------------------------------------------------------------
+
+            Console.WriteLine("\nAll employees grouped by department");
+
+            var employeesByDept = empTree.GroupBy(e => e.Department);
+
+            // All employees grouped by department
+            // Department: IT
+            //         Kim Abercrombie
+            //         Charlie Herb
+            // Department: Marketing
+            //         Jeff Hay
+            //         Tim Litton
+            // Department: Sales
+            //         Dave Barnet
+            //         Chris Preston
+
+            foreach (var dept in employeesByDept)
+            {
+                Console.WriteLine("Department: {0}", dept.Key);
+
+                foreach (var emp in dept)
+                {
+                    Console.WriteLine("\t{0} {1}", emp.FirstName, emp.LastName);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
